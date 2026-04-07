9d0765b582647355229fcd770c5dc2fd80f2e0fb
Bornita Chowdhury <bornitac07@gmail.com>
2026-04-06 20:22:14 -0400

    Add files via upload

---
+++ jstest/js/script.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+  let number = 6;
+
+  let button = document.querySelector("#myButton");
+  let textElement = document.querySelector("#numberText");
+
+  function changeNumber() {
+    number = 7;
+    textElement.innerHTML = number;
+
+    document.body.style.backgroundColor = "lightgreen";
+  }
+
+  button.addEventListener("click", changeNumber);
+
+});
